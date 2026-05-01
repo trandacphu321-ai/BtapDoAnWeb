f4c8e8c4843a73c319ffd9e2d79c2c7094102d70
dacphulab5 <trandacphu321@gmail.com>
2026-05-01 10:00:22 +0700

    Move monkey patch to app.py and revert vercel.json

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+import flask.json
+try:
+    from flask.json import JSONEncoder
+except ImportError:
+    import json
+    class JSONEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if hasattr(obj, 'to_json'):
+                return obj.to_json()
+            return super().default(obj)
+    flask.json.JSONEncoder = JSONEncoder
+    from flask import Flask
+    Flask.json_encoder = JSONEncoder
+
 from shop import app
 
 if __name__ == "__main__":
